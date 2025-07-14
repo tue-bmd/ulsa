e13de24b4c84acc5da198a633e37fb441d0914ca
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-07-14 11:45:36 +0000

    Play with doppler

--- ulsa/ops.py
+++ ulsa/ops.py
@@ -1,4 +1,5 @@
 import jax
+import numpy as np
 from keras import ops
 
 import zea.ops
@@ -15,6 +16,62 @@ def apply_along_axis(func, axis, arr):
         func = jax.vmap(func)
     result = func(arr)
     return ops.moveaxis(result, -1, axis)
+
+
+def iq2doppler(
+    data,
+    center_frequency,
+    pulse_repetition_frequency,
+    sound_speed,
+    hamming_size=None,
+    lag=1,
+):
+    """Compute Doppler from packet of I/Q Data.
+
+    Args:
+        data (ndarray): I/Q complex data of shape (n_z, n_x, n_frames).
+            n_frames corresponds to the ensemble length used to compute
+            the Doppler signal.
+
+    Returns:
+        doppler_velocities (ndarray): Doppler velocity map of shape (n_z, n_x).
+
+    """
+    assert data.ndim == 3, "Data must be a 3-D array"
+    assert isinstance(lag, int) and lag >= 0, "Lag must be a positive integer"
+
+    if hamming_size is None:
+        hamming_size = np.array([1, 1])
+    elif np.isscalar(hamming_size):
+        hamming_size = np.array([hamming_size, hamming_size])
+    assert hamming_size.all() > 0 and np.all(hamming_size == np.round(hamming_size)), (
+        "hamming_size must contain integers > 0"
+    )
+
+    # Auto-correlation method
+    iq1 = data[:, :, : data.shape[-1] - lag]
+    iq2 = data[:, :, lag:]
+    autocorr = ops.sum(iq1 * ops.conj(iq2), axis=2)  # Ensemble auto-correlation
+    autocorr = ops.convert_to_numpy(autocorr)
+
+    # Spatial weighted average
+    if hamming_size[0] != 1 and hamming_size[1] != 1:
+        h_row = np.hamming(hamming_size[0])
+        h_col = np.hamming(hamming_size[1])
+        autocorr = apply_along_axis(
+            lambda x: ops.correlate(x, h_row, mode="same"), 0, autocorr
+        )
+        autocorr = apply_along_axis(
+            lambda x: ops.correlate(x, h_col, mode="same"), 1, autocorr
+        )
+
+    # Doppler velocity
+    nyquist_velocities = (
+        sound_speed * pulse_repetition_frequency / (4 * center_frequency * lag)
+    )
+    doppler_velocities = -nyquist_velocities * ops.imag(ops.log(autocorr)) / np.pi
+
+    return doppler_velocities
 
 
 class AntiAliasing(zea.ops.Operation):
