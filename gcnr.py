c6f26ecf8d00f67803dabdd56657279539afd064
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-07-29 12:32:00 +0000

    Add gcnr valve over time

--- gcnr.py
+++ gcnr.py
@@ -54,23 +54,41 @@ black_masks = np.load(
 white_masks = np.load(
     f"{DATA_ROOT}/zea_20240701_P1_A4CH_0001_target_interpolated_masks_white.npy"
 )
+valve_masks = np.load(
+    f"{DATA_ROOT}/zea_20240701_P1_A4CH_0001_target_selected_interpolated_masks_valve.npy"
+)
+selected_frames = np.load(f"{DATA_ROOT}/20240701_P1_A4CH_0001_selected_frames.npy")[:-1]
 black_masks = black_masks > 0
 white_masks = white_masks > 0
+valve_masks = valve_masks > 0
+
+case = "valve"  # "normal", or "valve"
+if case == "valve":
+    black_masks = black_masks[selected_frames]
+    white_masks = white_masks[selected_frames]
+    targets = targets[selected_frames]
+    diverging_images = diverging_images[selected_frames]
+    reconstructions = reconstructions[selected_frames]
+    masks1 = black_masks
+    masks2 = valve_masks
+else:
+    mask1 = black_masks
+    mask2 = white_masks
 
 gcnr_all = {}
 
 print("gCNR for targets:")
-gcnr = gcnr_per_frame(targets, black_masks, white_masks)
+gcnr = gcnr_per_frame(targets, masks1, masks2)
 print(np.mean(gcnr))
 gcnr_all["targets"] = gcnr
 
 print("gCNR for diverging images:")
-gcnr = gcnr_per_frame(diverging_images, black_masks, white_masks)
+gcnr = gcnr_per_frame(diverging_images, masks1, masks2)
 print(np.mean(gcnr))
 gcnr_all["diverging_images"] = gcnr
 
 print("gCNR for reconstructions:")
-gcnr = gcnr_per_frame(reconstructions, black_masks, white_masks)
+gcnr = gcnr_per_frame(reconstructions, masks1, masks2)
 print(np.mean(gcnr))
 gcnr_all["reconstructions"] = gcnr
 
@@ -87,3 +105,36 @@ plotter.plot(
     metric_name="gCNR",
     context="styles/ieee-tmi.mplstyle",
 )
+
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(
+    selected_frames,
+    gcnr_all["targets"],
+    label=group_names["targets"],
+    marker="o",
+    # linestyle="",
+)
+plt.plot(
+    selected_frames,
+    gcnr_all["diverging_images"],
+    label=group_names["diverging_images"],
+    marker="o",
+    # linestyle="",
+)
+plt.plot(
+    selected_frames,
+    gcnr_all["reconstructions"],
+    label=group_names["reconstructions"],
+    marker="o",
+    # linestyle="",
+)
+plt.xlabel("Frame Index")
+plt.ylabel("gCNR")
+plt.title("gCNR per Frame")
+plt.legend()
+plt.savefig("gcnr_per_frame.png", dpi=300, bbox_inches="tight")
+
+zea.log.info(f"Saved gCNR plot as {zea.log.yellow('gcnr_per_frame.png')}")
