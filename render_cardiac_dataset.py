e82cb07db56aa433fe55fb46f1573cf998dd4ee5
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-07-29 14:42:29 +0000

    Add script to beamform A4CH cardiac data

---
+++ render_cardiac_dataset.py
@@ -0,0 +1,156 @@
+import os
+import sys
+
+import zea
+
+if __name__ == "__main__":
+    os.environ["KERAS_BACKEND"] = "jax"
+    zea.init_device(allow_preallocate=False)
+    sys.path.append("/ulsa")
+
+import jax
+import numpy as np
+import scipy.signal
+from keras import ops
+from tqdm import tqdm
+
+from active_sampling_temporal import preload_data
+from ulsa.ops import FirFilter, LowPassFilter, WaveletDenoise, lines_rx_apo
+from zea.display import compute_scan_convert_2d_coordinates, scan_convert_2d
+from zea.utils import translate
+
+
+class GetAutoDynamicRange(zea.ops.Operation):
+    def __init__(self, low_pct=18, high_pct=95):
+        super().__init__()
+        self.low_pct = low_pct
+        self.high_pct = high_pct
+
+    def call(self, dynamic_range=None, **kwargs):
+        data = kwargs[self.key]
+
+        vmin = dynamic_range[0] if dynamic_range else None
+        vmax = dynamic_range[1] if dynamic_range else None
+
+        if vmin is None:
+            vmin = ops.quantile(data, self.low_pct / 100)
+        if vmax is None:
+            vmax = ops.quantile(data, self.high_pct / 100)
+
+        vmin_db = 20 * ops.log10(vmin)
+        vmax_db = 20 * ops.log10(vmax)
+        dynamic_range = [vmin_db, vmax_db]
+        return {"dynamic_range": dynamic_range}
+
+
+def focused_waves(
+    target_sequence,
+    n_frames,
+    dynamic_range=[-70, -10],
+    grid_width=90,
+    resize_height=112,
+):
+    pipeline = zea.Pipeline(
+        [
+            FirFilter(axis=-3, filter_key="bandpass_rf"),
+            # WaveletDenoise(),  # optional
+            zea.ops.Demodulate(),
+            LowPassFilter(complex_channels=True, axis=-2),  # optional
+            zea.ops.Downsample(2),  # optional
+            zea.ops.PatchedGrid(
+                [
+                    zea.ops.TOFCorrection(),
+                    # zea.ops.PfieldWeighting(),  # optional
+                    zea.ops.DelayAndSum(),
+                ]
+            ),
+            zea.ops.EnvelopeDetect(),
+            zea.ops.Normalize(),
+            GetAutoDynamicRange(),
+            zea.ops.LogCompress(),
+            zea.ops.Lambda(lambda x: ops.expand_dims(x, axis=-1)),
+            zea.ops.Lambda(
+                ops.image.resize,
+                {
+                    "size": (resize_height, grid_width),
+                    "interpolation": "bilinear",
+                    "antialias": True,
+                },
+            ),
+            zea.ops.Lambda(lambda x: ops.squeeze(x, axis=-1)),
+        ],
+        with_batch_dim=False,
+        jit_options="ops",
+    )
+
+    with zea.File(target_sequence) as file:
+        raw_data_sequence, scan, probe = preload_data(
+            file,
+            n_frames,
+            data_type="data/raw_data",
+            type="focused",
+        )
+
+    bandpass_rf = scipy.signal.firwin(
+        numtaps=128,
+        cutoff=np.array([0.5, 1.5]) * scan.center_frequency,
+        pass_zero="bandpass",
+        fs=scan.sampling_frequency,
+    )
+    scan.polar_limits = list(np.deg2rad([-45, 45]))
+    scan.grid_size_x = grid_width
+    scan.dynamic_range = None
+    rx_apo = lines_rx_apo(scan.n_tx, scan.grid_size_z, scan.grid_size_x)
+    params = pipeline.prepare_parameters(
+        scan=scan,
+        bandwidth=2e6,
+        bandpass_rf=bandpass_rf,
+        rx_apo=rx_apo,
+    )
+    scan_convert_2d_jit = jax.jit(
+        scan_convert_2d, static_argnames=("fill_value", "order")
+    )
+
+    image_shape = (resize_height, grid_width)
+    coords, _ = compute_scan_convert_2d_coordinates(
+        image_shape=image_shape,
+        rho_range=(0, image_shape[0]),
+        theta_range=scan.theta_range,
+        resolution=0.1,
+    )
+
+    images = []
+    for raw_data in tqdm(raw_data_sequence):
+        output = pipeline(data=raw_data, **params)
+        image = output.pop("data")
+        params["maxval"] = output.pop("maxval")
+        image, _ = scan_convert_2d_jit(
+            image, coordinates=coords, fill_value=np.nan, order=0
+        )
+        images.append(ops.convert_to_numpy(image))
+    images = np.stack(images, axis=0)
+
+    dynamic_range = output["dynamic_range"]
+    return images, dynamic_range
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    save_dir = Path("/mnt/z/usbmd/Wessel/cardiac_mp4s")
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    folder = Path("/mnt/USBMD_datasets/2024_USBMD_cardiac_S51/HDF5/")
+    # find all a4ch files
+    files = list(folder.glob("*_A4CH_*.hdf5"))
+    n_frames = 50
+
+    dynamic_range = [-70, -10]
+    for file in files:
+        images, dynamic_range = focused_waves(
+            file, n_frames, dynamic_range=dynamic_range
+        )
+        _images = translate(images, dynamic_range, (0, 255))
+        _images = np.clip(_images, 0, 255).astype(np.uint8)
+        mp4_path = (save_dir / file.stem).with_suffix(".mp4")
+        zea.utils.save_to_mp4(_images, mp4_path, fps=30)
