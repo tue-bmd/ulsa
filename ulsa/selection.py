691a09da4d40ff52f6ea44bd16fdd64dd2e2d3d7
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2025-07-31 14:16:27 +0000

    Ruff

--- ulsa/selection.py
+++ ulsa/selection.py
@@ -4,16 +4,16 @@ Prototypes for additional `zea` selection functions
 
 """
 
-from keras import ops
-import numpy as np
 import jax
+import numpy as np
+from keras import ops
 
+from ulsa.downstream_task import DifferentiableDownstreamTask, downstream_task_registry
+from zea.agent import masks
 from zea.agent.selection import GreedyEntropy, LinesActionModel
 from zea.backend.autograd import AutoGrad
-from zea.agent import masks
 from zea.internal.registry import action_selection_registry
 
-from ulsa.downstream_task import downstream_task_registry, DifferentiableDownstreamTask
 
 @action_selection_registry(name="downstream_task_selection")
 class DownstreamTaskSelection(GreedyEntropy):
@@ -21,6 +21,7 @@ class DownstreamTaskSelection(GreedyEntropy):
     Select lines to maximize information gain with respect to a
     downstream task outcome.
     """
+
     def __init__(
         self,
         n_actions: int,
@@ -36,18 +37,30 @@ class DownstreamTaskSelection(GreedyEntropy):
         """
         downstream_task_model should be differentiable
         """
-        super().__init__(n_actions, n_possible_actions, img_width, img_height, mean, std_dev, num_lines_to_update)
-         # NOTE: for echonet don't forget to wrap scan convert in here
-         # TODO: batch_size=4 shouldn't be hard coded
-        self.downstream_task: DifferentiableDownstreamTask = (downstream_task_registry[downstream_task_key])(batch_size=4)
+        super().__init__(
+            n_actions,
+            n_possible_actions,
+            img_width,
+            img_height,
+            mean,
+            std_dev,
+            num_lines_to_update,
+        )
+        # NOTE: for echonet don't forget to wrap scan convert in here
+        # TODO: batch_size=4 shouldn't be hard coded
+        self.downstream_task: DifferentiableDownstreamTask = (
+            downstream_task_registry[downstream_task_key]
+        )(batch_size=4)
 
     def compute_output_and_saliency_propagation_hutchinson(self, particles, key):
         num_hutchinson_samples = 5
 
         z_squared = ops.zeros(self.downstream_task.output_shape)
-        for i in range(num_hutchinson_samples): # TODO: vmap
+        for i in range(num_hutchinson_samples):  # TODO: vmap
             subkey = jax.random.fold_in(key, i)
-            v = jax.random.normal(subkey, self.downstream_task) # TODO: downstream task output shape?
+            v = jax.random.normal(
+                subkey, self.downstream_task
+            )  # TODO: downstream task output shape?
             # vjp returns a function that computes J^T v
             _, vjp_fun = jax.vjp(self.downstream_task.call_differentiable, particles)
             jt_v = vjp_fun(v)[0]  # [0] to get the input tangent
@@ -55,7 +68,9 @@ class DownstreamTaskSelection(GreedyEntropy):
 
         posterior_variance = ops.expand_dims(ops.var(particles, axis=0), axis=0)
 
-        return posterior_variance * z_squared # TODO: where does z_squared get rid of the particle dim?
+        return (
+            posterior_variance * z_squared
+        )  # TODO: where does z_squared get rid of the particle dim?
 
     def compute_output_and_saliency_propagation_summed(self, particles):
         autograd = AutoGrad()
@@ -73,11 +88,16 @@ class DownstreamTaskSelection(GreedyEntropy):
             ops.mean(ops.abs(grads), axis=0), axis=0
         )
         return posterior_variance * mean_absolute_jacobian
-    
+
     def sum_neighbouring_columns_into_n_possible_actions(self, full_linewise_salience):
         full_image_width = ops.shape(full_linewise_salience)[1]
-        assert full_image_width % self.n_possible_actions == 0, "n_possible_actions must divide evenly into image width"
-        stacked_linewise_salience = ops.reshape(full_linewise_salience, (self.n_possible_actions, full_image_width // self.n_possible_actions))
+        assert full_image_width % self.n_possible_actions == 0, (
+            "n_possible_actions must divide evenly into image width"
+        )
+        stacked_linewise_salience = ops.reshape(
+            full_linewise_salience,
+            (self.n_possible_actions, full_image_width // self.n_possible_actions),
+        )
         return ops.sum(stacked_linewise_salience, axis=1)[None, ...]
 
     def sample(self, particles):
@@ -91,30 +111,49 @@ class DownstreamTaskSelection(GreedyEntropy):
                 - Newly selected lines as k-hot vectors, shaped (batch_size, n_possible_actions)
                 - Masks of shape (batch_size, img_height, img_width)
         """
-        particles = ops.expand_dims(particles[0], axis=-1) # remove batch, add channel
-        pixelwise_contribution_to_var_dst = self.compute_output_and_saliency_propagation_summed(particles)
-        linewise_contribution_to_var_dst = ops.sum(pixelwise_contribution_to_var_dst[...,0], axis=1)
-        linewise_contribution_to_var_dst = self.sum_neighbouring_columns_into_n_possible_actions(linewise_contribution_to_var_dst)
+        particles = ops.expand_dims(particles[0], axis=-1)  # remove batch, add channel
+        pixelwise_contribution_to_var_dst = (
+            self.compute_output_and_saliency_propagation_summed(particles)
+        )
+        linewise_contribution_to_var_dst = ops.sum(
+            pixelwise_contribution_to_var_dst[..., 0], axis=1
+        )
+        linewise_contribution_to_var_dst = (
+            self.sum_neighbouring_columns_into_n_possible_actions(
+                linewise_contribution_to_var_dst
+            )
+        )
 
         # Greedily select best line, reweight entropies, and repeat
         all_selected_lines = []
         for _ in range(self.n_actions):
-            max_contribution_line, linewise_contribution_to_var_dst = ops.vectorized_map(
-                self.select_line_and_reweight_entropy, linewise_contribution_to_var_dst
+            max_contribution_line, linewise_contribution_to_var_dst = (
+                ops.vectorized_map(
+                    self.select_line_and_reweight_entropy,
+                    linewise_contribution_to_var_dst,
+                )
             )
             all_selected_lines.append(max_contribution_line)
 
         selected_lines_k_hot = ops.any(
-            ops.one_hot(all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE),
+            ops.one_hot(
+                all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE
+            ),
             axis=0,
         )
-        return selected_lines_k_hot, self.lines_to_im_size(selected_lines_k_hot), pixelwise_contribution_to_var_dst
+        return (
+            selected_lines_k_hot,
+            self.lines_to_im_size(selected_lines_k_hot),
+            pixelwise_contribution_to_var_dst,
+        )
+
 
 @action_selection_registry(name="greedy_variance")
 class GreedyVariance(GreedyEntropy):
     """
     Greedily select the lines with maximum variance.
     """
+
     def __init__(
         self,
         n_actions: int,
@@ -126,9 +165,16 @@ class GreedyVariance(GreedyEntropy):
         num_lines_to_update: int = 5,
         average_across_batch: bool = False,
     ):
-        super().__init__(n_actions, n_possible_actions, img_width, img_height, mean, std_dev, num_lines_to_update)
+        super().__init__(
+            n_actions,
+            n_possible_actions,
+            img_width,
+            img_height,
+            mean,
+            std_dev,
+            num_lines_to_update,
+        )
         self.average_across_batch = average_across_batch
-
 
     def sample(self, particles):
         """Sample the action using the greedy variance method.
@@ -141,12 +187,14 @@ class GreedyVariance(GreedyEntropy):
                 - Newly selected lines as k-hot vectors, shaped (batch_size, n_possible_actions)
                 - Masks of shape (batch_size, img_height, img_width)
         """
-        pixelwise_variance = ops.var(particles, axis=1) # [batch_size, height, width]
-        # NOTE: we compute the linewise variance as the sum of pixelwise variances. 
+        pixelwise_variance = ops.var(particles, axis=1)  # [batch_size, height, width]
+        # NOTE: we compute the linewise variance as the sum of pixelwise variances.
         # Since we're using variance as our entropy, we have that H(X_1, X_2, ...) = H(X_1) + H(X_2) + ...
-        linewise_variance = ops.sum(pixelwise_variance, axis=1) # [batch_size, width]
+        linewise_variance = ops.sum(pixelwise_variance, axis=1)  # [batch_size, width]
         if self.average_across_batch:
-            linewise_variance = ops.mean(linewise_variance, axis=1) # for 3d case [1, width]
+            linewise_variance = ops.mean(
+                linewise_variance, axis=1
+            )  # for 3d case [1, width]
 
         # Greedily select best line, reweight entropies, and repeat
         all_selected_lines = []
@@ -157,16 +205,20 @@ class GreedyVariance(GreedyEntropy):
             all_selected_lines.append(max_contribution_line)
 
         selected_lines_k_hot = ops.any(
-            ops.one_hot(all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE),
+            ops.one_hot(
+                all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE
+            ),
             axis=0,
         )
         return selected_lines_k_hot, self.lines_to_im_size(selected_lines_k_hot)
-    
+
+
 @action_selection_registry(name="greedy_entropy_univariate_gaussian")
 class GreedyEntropyUnivariateGaussian(GreedyEntropy):
     """
     Greedily select the lines with maximum variance.
     """
+
     def __init__(
         self,
         n_actions: int,
@@ -178,14 +230,21 @@ class GreedyEntropyUnivariateGaussian(GreedyEntropy):
         num_lines_to_update: int = 5,
         average_across_batch: bool = False,
     ):
-        super().__init__(n_actions, n_possible_actions, img_width, img_height, mean, std_dev, num_lines_to_update)
+        super().__init__(
+            n_actions,
+            n_possible_actions,
+            img_width,
+            img_height,
+            mean,
+            std_dev,
+            num_lines_to_update,
+        )
         self.average_across_batch = average_across_batch
-
 
     def sample(self, particles):
         """Sample the action using the greedy entropy method with univariate Gaussian assumption.
         See 'Entropy' here: https://en.wikipedia.org/wiki/Normal_distribution
-        NOTE: this is designed for line-based subsampling, i.e. where self.img_height = |A^\ell|
+        NOTE: this is designed for line-based subsampling, i.e. where self.img_height = |A^l|
             - See `derivations/Pixelwise_Gaussian_Entropy.pdf` for more details.
 
         Args:
@@ -196,12 +255,16 @@ class GreedyEntropyUnivariateGaussian(GreedyEntropy):
                 - Newly selected lines as k-hot vectors, shaped (batch_size, n_possible_actions)
                 - Masks of shape (batch_size, img_height, img_width)
         """
-        pixelwise_variance = 0.5*ops.log(((2 * np.pi * np.e)**self.img_height) * ops.var(particles, axis=1)) # [batch_size, height, width]
-        # NOTE: we compute the linewise variance as the sum of pixelwise variances. 
+        pixelwise_variance = 0.5 * ops.log(
+            ((2 * np.pi * np.e) ** self.img_height) * ops.var(particles, axis=1)
+        )  # [batch_size, height, width]
+        # NOTE: we compute the linewise variance as the sum of pixelwise variances.
         # Since we're using variance as our entropy, we have that H(X_1, X_2, ...) = H(X_1) + H(X_2) + ...
-        linewise_variance = ops.sum(pixelwise_variance, axis=1) # [batch_size, width]
+        linewise_variance = ops.sum(pixelwise_variance, axis=1)  # [batch_size, width]
         if self.average_across_batch:
-            linewise_variance = ops.mean(linewise_variance, axis=1) # for 3d case [1, width]
+            linewise_variance = ops.mean(
+                linewise_variance, axis=1
+            )  # for 3d case [1, width]
 
         # Greedily select best line, reweight entropies, and repeat
         all_selected_lines = []
@@ -212,11 +275,14 @@ class GreedyEntropyUnivariateGaussian(GreedyEntropy):
             all_selected_lines.append(max_contribution_line)
 
         selected_lines_k_hot = ops.any(
-            ops.one_hot(all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE),
+            ops.one_hot(
+                all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE
+            ),
             axis=0,
         )
         return selected_lines_k_hot, self.lines_to_im_size(selected_lines_k_hot)
-    
+
+
 @action_selection_registry(name="greedy_entropy_fixed")
 class GreedyEntropyFixed(LinesActionModel):
     """Greedy entropy action selection.
@@ -291,15 +357,21 @@ class GreedyEntropyFixed(LinesActionModel):
             Tensor: batch of pixelwise pairwise Gaussian errors,
             of shape (n_particles, n_particles, batch, height, width)
         """
-        assert particles.shape[1] > 1, "The entropy cannot be approximated using a single particle."
+        assert particles.shape[1] > 1, (
+            "The entropy cannot be approximated using a single particle."
+        )
 
         if n_possible_actions is None:
             n_possible_actions = particles.shape[-1]
 
         # TODO: I think we only need to compute the lower triangular
         # of this matrix, since it's symmetric
-        squared_l2_error_matrices = (particles[:, :, None, ...] - particles[:, None, :, ...]) ** 2
-        gaussian_error_per_pixel_i_j = ops.exp(- (squared_l2_error_matrices) / (2 * entropy_sigma**2))
+        squared_l2_error_matrices = (
+            particles[:, :, None, ...] - particles[:, None, :, ...]
+        ) ** 2
+        gaussian_error_per_pixel_i_j = ops.exp(
+            -(squared_l2_error_matrices) / (2 * entropy_sigma**2)
+        )
         # Vertically stack all columns corresponding with the same line
         # This way we can just sum across the height axis and get the entropy
         # for each pixel in a given line
@@ -333,11 +405,13 @@ class GreedyEntropyFixed(LinesActionModel):
         Returns:
             Tensor: batch of entropies per line, of shape (batch, n_possible_actions)
         """
-        gaussian_error_per_pixel_stacked = GreedyEntropy.compute_pairwise_pixel_gaussian_error(
-            particles,
-            self.stack_n_cols,
-            self.n_possible_actions,
-            self.entropy_sigma,
+        gaussian_error_per_pixel_stacked = (
+            GreedyEntropy.compute_pairwise_pixel_gaussian_error(
+                particles,
+                self.stack_n_cols,
+                self.n_possible_actions,
+                self.entropy_sigma,
+            )
         )
         # sum out first dimension of (n_particles x n_particles) error matrix
         # [n_particles, batch, height, width]
@@ -420,7 +494,9 @@ class GreedyEntropyFixed(LinesActionModel):
             all_selected_lines.append(max_entropy_line)
 
         selected_lines_k_hot = ops.any(
-            ops.one_hot(all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE),
+            ops.one_hot(
+                all_selected_lines, self.n_possible_actions, dtype=masks._DEFAULT_DTYPE
+            ),
             axis=0,
         )
         return selected_lines_k_hot, self.lines_to_im_size(selected_lines_k_hot)
