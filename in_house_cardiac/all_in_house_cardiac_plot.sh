dda1bcc895c84d5c2a1b92be6530d4723139d89a
Wessel van Nierop <40076995+wesselvannierop@users.noreply.github.com>
2026-01-19 14:37:59 +0000

    Move and rename

--- in_house_cardiac/all_in_house_cardiac_plot.sh
+++
@@ -1,3 +0,0 @@
-python in_house_cardiac/gncr_paper.py # gcnr violin
-python in_house_cardiac/gcnr.py # gcnr over time
-python plotting/plot_in_house_cardiac.py # qualitative plots
---
+++ plotting/all_in_house_cardiac_plot.sh
@@ -0,0 +1,4 @@
+python plotting/plot_gncr_in_house.py # gcnr violin
+python ulsa/in_house_cardiac/gcnr.py # gcnr over time
+python plotting/qualitative_in_house.py # qualitative plots
+python plotting/plot_psnr_in_house.py
